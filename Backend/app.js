2edfadd9133db4ff6263293dc3d25e2f121e9997
abhishek <abhisheksng.dl@gmail.com>
2023-11-20 17:50:00 +0530

    new changes

--- Backend/app.js
+++ Backend/app.js
@@ -26,6 +26,16 @@ const spv = require("./routes/spv.route.js");
 const attorney = require("./routes/attorney.route.js");
 var createError = require("http-errors");
 app.use(logger("dev"));
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
