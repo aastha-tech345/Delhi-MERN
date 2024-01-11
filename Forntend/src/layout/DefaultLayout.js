8ea30ad72e739a0f0affac34df8b2ff0f5aeaea6
dnesmamgain <iamdineshmamgain@gmail.com>
2024-01-11 18:09:03 +0530

    refactor: customer listing

--- Forntend/src/layout/DefaultLayout.js
+++ Forntend/src/layout/DefaultLayout.js
@@ -10,7 +10,7 @@ const DefaultLayout = () => {
       <AppSidebar />
       <div className="wrapper min-vh-100 bg-light">
         <AppHeader />
-        <div className="body flex-grow-1 px-3">
+        <div className="body flex-grow-1 px-md-3">
           <AppContent />
         </div>
         <AppFooter />
