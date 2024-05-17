fc5eaa58722f6389c5181bc69e71a25acf46b73c
dluser5 <aasthamodanwal.dl@gmail.com>
2024-05-17 14:35:08 +0530

    save update123

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -13,5 +13,5 @@ const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", () => {
-  console.log("Connected to the database!");
+  console.log("Connected to the database12!");
 });
